c367152698f5374477b690dd0428ed3a047ff3d0
Ben <110583491+TheLastBen@users.noreply.github.com>
2022-11-25 21:10:25 +0100

    remove unnecessary models

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -740,6 +740,7 @@ def main():
                         subprocess.call('cp -f '+frz_dir +'/*.* '+ save_dir+'/text_encoder', shell=True)                     
                      chkpth=args.Session_dir+"/"+inst+".ckpt"
                      subprocess.call('python /content/diffusers/scripts/convert_diffusers_to_original_stable_diffusion.py --model_path ' + save_dir + ' --checkpoint_path ' + chkpth + ' --half', shell=True)
+                     subprocess.call('rm -r '+ save_dir, shell=True)
                      i=i+args.save_n_steps
             
         accelerator.wait_for_everyone()
